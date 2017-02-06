0df2a081e558df0d956cb5c44f783b09d8df5266
Joanne Yeung <jttyeung@gmail.com>
2017-02-06 13:02:34 -0800

    made quotations consistent

--- server.py
+++ server.py
@@ -15,32 +15,32 @@ app.jinja_env.auto_reload = True
 @app.route('/')
 def homepage():
     """ Brings user to the homepage. """
-    return render_template("index.html")
+    return render_template('index.html')
 
 
 @app.route('/login')
 def login():
     """ Brings user to the login page. """
-    return render_template("login.html")
+    return render_template('login.html')
 
 
 @app.route('/register', methods=['GET'])
 def register():
     """ Brings user to the registration page. """
-    return render_template("registration.html")
+    return render_template('registration.html')
 
 
-@app.route('/register', methods=['POST'])
-def add_user_registration():
-    """ Processes a new user registration. """
+# @app.route('/register', methods=['POST'])
+# def add_user_registration():
+#     """ Processes a new user registration. """
 
-    if username exists:
-        flash('Sorry, that email has already been registered. Login instead?')
-        return redirect('/register')
+#     if username exists:
+#         flash('Sorry, that email has already been registered. Login instead?')
+#         return redirect('/register')
 
-    else create a new user:
-        flash('You have successfully created an account. Please login to continue.')
-        return redirect('/')
+#     else create a new user:
+#         flash('You have successfully created an account. Please login to continue.')
+#         return redirect('/')
 
 @app.route('/account')
 def account_details():
