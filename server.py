70cf5a439bffd80cc63f819cbe5034d12d282828
Joanne Yeung <jttyeung@gmail.com>
2017-02-06 12:50:59 -0800

    setup server structure and server integration testing

---
+++ server.py
@@ -0,0 +1,58 @@
+""" Investable Server """
+
+from flask import Flask, render_template, flash
+# from flask_debugtoolbar import DebugToolbarExtension
+import jinja2
+
+
+app = Flask(__name__)
+
+# jinja debugger
+app.jinja_env.undefined = jinja2.StrictUndefined
+app.jinja_env.auto_reload = True
+
+
+@app.route('/')
+def homepage():
+    """ Brings user to the homepage. """
+    return render_template("index.html")
+
+
+@app.route('/login')
+def login():
+    """ Brings user to the login page. """
+    return render_template("login.html")
+
+
+@app.route('/register', methods=['GET'])
+def register():
+    """ Brings user to the registration page. """
+    return render_template("registration.html")
+
+
+@app.route('/register', methods=['POST'])
+def add_user_registration():
+    """ Processes a new user registration. """
+
+    if username exists:
+        flash('Sorry, that email has already been registered. Login instead?')
+        return redirect('/register')
+
+    else create a new user:
+        flash('You have successfully created an account. Please login to continue.')
+        return redirect('/')
+
+@app.route('/account')
+def account_details():
+    """ Shows a user's account details. """
+
+    return render_template('account.html')
+
+
+if __name__ == "__main__":
+    app.debug = True
+
+    # Use the DebugToolbar
+    # DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0", port=4000)
---
+++ test-server.py
@@ -0,0 +1,64 @@
+import unittest
+import server
+
+class InvestableServerTest(unittest.TestCase):
+    """ Integration testing for Investable's server. """
+
+
+    def setUp(self):
+        """ Sets up new test client per test. """
+        self.client = server.app.test_client()
+        server.app.config['TESTING'] = True
+
+
+    def test_homepage(self):
+        """ Test that the homepage loads. """
+        result = self.client.get('/')
+        self.assertEqual(result.status_code, 200)
+        # add assertIn
+
+
+    def test_login(self):
+        """ Test that the login page loads. """
+        result = self.client.get('/login')
+        self.assertEqual(result.status_code, 200)
+        # add assertIn
+
+
+    def test_login(self):
+        """ Test that the user is able to login. """
+        result = self.client.post('/login')
+        self.assertEqual(result.status_code, 200)
+        # add assertIn
+
+
+    def test_register(self):
+        """ Test that the registration page loads. """
+        result = self.client.get('/register')
+        self.assertEqual(result.status_code, 200)
+        # add assertIn
+
+
+    def test_registration_success(self):
+        """ Test that the user is able to successfully register. """
+        result = self.client.post('/register')
+        self.assertEqual(result.status_code, 200)
+        # add assertIn
+
+
+    def test_registration_invalid(self):
+        """ Test that the user cannot create an account if it already exists in the database. """
+        result = self.client.post('/register')
+        self.assertEqual(result.status_code, 200)
+        # add assertIn
+
+
+    def test_account_details(self):
+        """ Test that user account detail page loads. """
+        result = self.client.get('/account')
+        self.assertEqual(result.status_code, 200)
+        # add assertIn
+
+
+if __name__ = '__main__':
+    unittest.main()
