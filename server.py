47d9b5b31037c7255aa03bd9b897a388c4c0ac36
Joanne Yeung <jttyeung@gmail.com>
2017-02-06 17:09:29 -0800

    added search functionality that connects to the zillow api

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
 """ Investable Server """
 
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, redirect, flash, request
 from flask_debugtoolbar import DebugToolbarExtension
+
 import jinja2
 import os
+import urllib
 
 
 app = Flask(__name__)
 
 app.secret_key = os.environ['APP_KEY']
+app.zwsid = os.environ['ZWSID']
 
 # jinja debugger
 app.jinja_env.undefined = jinja2.StrictUndefined
@@ -25,9 +28,15 @@ def homepage():
 @app.route('/search')
 def search():
     """ Returns user search results. """
-
+    address = request.args.get('address-search')
+    citystatezip = request.args.get('citystatezip-search')
+    search_location = {'address': address, 'citystatezip': citystatezip}
+    result = 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=' + app.zwsid + '&' + urllib.urlencode(search_location)
     # need to determine where search lands user - same page, refreshed page, etc.
-    return render_template('/')
+    return result
+
+    # try to figure out how to get XML <message><code> from resulting api call. '0' means 'Request successfully processed', but what if call returns no results?
+    # code 508 is 'Error: no exact match found for input address'
 
 
 @app.route('/login')
@@ -55,6 +64,7 @@ def register():
 #     else create a new user:
 #         flash('You have successfully created an account. Please login to continue.')
 #         return redirect('/')
+
 
 @app.route('/account')
 def account_details():
