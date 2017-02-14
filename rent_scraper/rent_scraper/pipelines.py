4f02629a6c5e4524a7c147837acc918bb94ad865
Joanne Yeung <jttyeung@gmail.com>
2017-02-13 16:30:19 -0800

    built data scrubbing pipeline for scraper

--- rent_scraper/rent_scraper/pipelines.py
+++ rent_scraper/rent_scraper/pipelines.py
@@ -1,11 +1,30 @@
+""" Scrubs scraped data of whitespaces and interesting characters and returns it into a data-maintenance friendly output. """
+
 # -*- coding: utf-8 -*-
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.exceptions import DropItem
+
+import re
 
 
 class RentScraperPipeline(object):
     def process_item(self, item, spider):
+        if item['price']:
+            item['price'] = int(re.sub('[^\d.]+', '', ''.join(item['price'])))
+        else:
+            raise DropItem('Missing price in %s' % item)
+
+        if item['bedrooms']:
+            bedrooms, sqft = ''.join(item['bedrooms']).split('br')
+            item['bedrooms'] = int(re.sub('[^\d.]+', '', bedrooms))
+            item['sqft'] = re.sub('[^\d.]+', '', sqft)
+                if item['sqft']:
+                    item['sqft'] = int(item['sqft'])
+        else:
+            item['bedrooms'] = ''
+            item['sqft'] = ''
+
+        if item['neighborhood']:
+            item['neighborhood'] = re.sub('[^\s\w.]+', '', ''.join(item['neighborhood'])).rstrip().lstrip()
+
         return item
