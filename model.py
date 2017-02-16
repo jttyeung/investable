63b0f5368da51b938f7e8ad171cfde4b07ee383b
Joanne Yeung <jttyeung@gmail.com>
2017-02-15 16:09:19 -0800

    updated model to fit new scraping information

--- model.py
+++ model.py
@@ -25,70 +25,62 @@ class User(db.Model):
         return '<User id=%s firstname=%s lastname=%s email=%s>' % (self.user_id, self.firstname, self.lastname, self.email)
 
 
-class Home(db.Model):
-    """ Home listings. """
+class UnitDetails(db.Model):
+    """ Details of the unit for sale or up for rent. """
 
-    __tablename__ = 'homes'
+    __tablename__ = 'unitdetails'
 
-    home_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    description = db.Column(db.String(250), nullable=False)
-    price = db.Column(db.Integer, nullable=False)
-    HOA = db.Column(db.Integer, nullable=True)
-    photo_url = db.Column(db.String(2083), nullable=True)
-    neighborhood_id = db.Column(db.Integer, db.ForeignKey(neighborhoods.neighborhood_id))
-    size_id = db.Column(db.Integer, db.ForeignKey(sizes.size_id))
-
+    detail_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    neighborhood = db.Column(db.String(150), nullable=False)
+    bedrooms = db.Column(db.Integer, nullable=True)
+    bathrooms = db.Column(db.Integer, nullable=True)
+    sqft = db.Column(db.Integer, nullable=True)
+    latitude = db.Column(db.Integer, nullable=False)
+    longitude = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        """ Shows information about the home. """
+        """ Shows information on the unit details of the unit up for rent or for sale. """
 
         return '<Home id=%s description=%s price=%s>' % (self.home_id, self.description, self.price)
 
 
-class Rental(db.Model):
-    """ Rental listings. """
+class ForSale(db.Model):
+    """ Unit sale listings. """
 
-    __tablename__ = 'rentals'
+    __tablename__ = 'selling'
 
-    cl_id = db.Column(db.String(150), primary_key=True)
+    home_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     price = db.Column(db.Integer, nullable=False)
-    date_saved = db.Column(db.DateTime, nullable=False)
-    # neighborhood_id = db.Column(db.Integer, db.ForeignKey(neighborhoods.neighborhood_id), nullable=False)
-    # size_id = db.Column(db.Integer, db.ForeignKey(sizes.size_id), nullable=False)
+    HOA = db.Column(db.Integer, nullable=True)
+    photo_url = db.Column(db.String(2083), nullable=True)
+    detail_id = db.Column(db.Integer, db.ForeignKey('unitdetails.detail_id'), nullable=False)
 
-
-class Neighborhood(db.Model):
-    """ Neighborhoods defined on Craigslist. """
-
-    __tablename__ = 'neighborhoods'
-
-    neighborhood_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(150), nullable=False)
+    unitdetails = db.relationship('UnitDetails', backref='selling')
 
 
     def __repr__(self):
-        """ Shows neighborhood information. """
+        """ Shows information about the unit up for sale. """
 
-        return '<Neighborhood id=%s name=%s>' % (self.neighborhood_id, self.name)
-
-
-# class Rent(db.Model):
-#     """ Rental listings. """
-
-#     __tablename__ = 'rents'
-
-#     rental_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+        return '<ForSale id=%s price=%s hoa=%s detail_id=%s>' % (self.home_id, self.price, self.hoa, self.detail_id)
 
 
-#     def __repr__(self):
-#         """ Shows average rent information. """
+class ForRent(db.Model):
+    """ Unit rental listings. """
 
-#         rent_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#         neighborhood_id = db.Column(db.Integer, db.ForeignKey(neighborhoods.neighborhood_id), nullable=False)
-#         size_id = db.Column(db.Integer, db.ForeignKey(sizes.size_id), nullable=False)
-#         avg_rent = db.Column(db.Integer, nullable=False)
+    __tablename__ = 'renting'
 
-#         return '<Rental id=%s avg_rent=%s size=%s neighborhood=%s>' % (self.rent_id, self.avg_rent, self.size_id, self.neighborhood_id)
+    cl_id = db.Column(db.String(150), primary_key=True)
+    price = db.Column(db.Integer, nullable=False)
+    date_posted = db.Column(db.TIMESTAMP(timezone=True), nullable=False)
+    detail_id = db.Column(db.Integer, db.ForeignKey('unitdetails.detail_id'), nullable=False)
+
+    unitdetails = db.relationship('UnitDetails', backref='renting')
+
+
+    def __repr__(self):
+        """ Shows information about the unit up for rent. """
+
+        return '<ForRent id=%s description=%s price=%s detail_id=%s>' % (self.cl_id, self.price, self.date_saved, self.detail_id)
 
 
 class Favorite(db.Model):
@@ -97,32 +89,17 @@ class Favorite(db.Model):
     __tablename__ = 'favorites'
 
     favorite_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(users.user_id), nullable=False)
-    home_id = db.Column(db.Integer, db.ForeignKey(homes.home_id), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    home_id = db.Column(db.Integer, db.ForeignKey('selling.home_id'), nullable=False)
     date_saved = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
+    users = db.relationship('User', backref='favorites')
+    selling = db.relationship('ForSale', backref='favorites')
 
     def __repr__(self):
         """ Shows the user's favorite homes. """
 
         return '<Favorite id=%s user_id=%s home_id=%s date_saved=%s>' % (self.favorite_id, self.user_id, self.home_id, self.date_saved)
-
-
-class Size(db.Model):
-    """ Home and rental sizes. """
-
-    __tablename__ = 'sizes'
-
-    size_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    bedrooms = db.Column(db.Integer, nullable=True)
-    bathrooms = db.Column(db.Integer, nullable=True)
-    sqft = db.Column(db.Integer, nullable=True)
-
-
-    def __repr__(self):
-        """ Shows the home and rental sizes. """
-
-        return '<Size id=%s bedrooms=%s bathrooms=%s sqft=%s>' % (self.size_id, self.bedrooms, self.bathrooms, self.sqft)
 
 
 
