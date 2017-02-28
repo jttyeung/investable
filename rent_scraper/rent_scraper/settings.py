76ca6dc737254a183d2450977dca72716b06ec06
Joanne Yeung <jttyeung@gmail.com>
2017-02-28 00:45:14 -0800

    updated scraper settings

--- rent_scraper/rent_scraper/settings.py
+++ rent_scraper/rent_scraper/settings.py
@@ -93,7 +93,7 @@ AUTOTHROTTLE_ENABLED = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = True
+# HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
