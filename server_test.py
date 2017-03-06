c17fb60d3b79a639a8b7ee1c7f49ba4cb8b12d64
Joanne Yeung <jttyeung@gmail.com>
2017-03-05 17:03:38 -0800

    moved database tests

--- server_test.py
+++ server_test.py
@@ -1,7 +1,7 @@
 import unittest
 from flask import session
-
-from model import connect_to_db_flask, db, sample_data, User, Favorite, Listing, Rental, UnitDetails
+import server
+from model import *
 from server import app
 
 
@@ -17,39 +17,43 @@ class FlaskServerIntegrationTests(unittest.TestCase):
 
 
     def test_homepage(self):
-        """ Test that the homepage loads. """
+        """ Test that the homepage route loads. """
 
         result = self.client.get('/')
         self.assertEqual(result.status_code, 200)
-        self.assertIn('Search a property by location or city:', result)
+        # import pdb; pdb.set_trace()
+        self.assertIn('<h1>Find rental property real estate that is <span id="appname">Investable</span>.', result.data)
+        self.assertIn('Search Again', result.data)
 
 
     def test_search(self):
-        """ Test that the search element returns a response. """
+        """ Test that the search route returns a response. """
 
+        app.zwsid = app.zwsid
         result = self.client.get('/search.json')
         self.assertEqual(result.status_code, 200)
 
 
-    def test_avg_rent(self):
-        """ Test that the search element returns a response. """
+    # def test_avg_rent(self):
+    #     """ Test that the average rent route returns a response. """
 
-        result = self.client.get('/avgrent.json')
-        self.assertEqual(result.status_code, 200)
-
-
-    def test_listings(self):
-        """ Test that the search element returns a response. """
-
-        result = self.client.get('/listings.json')
-        self.assertEqual(result.status_code, 200)
+    #     result = self.client.get('/avgrent.json')
+    #     self.assertEqual(result.status_code, 200)
 
 
-    def test_calculator(self):
-        """ Test that the search element returns a response. """
+    # def test_listings(self):
+    #     """ Test that the listings route returns a response. """
 
-        result = self.client.get('/calculator')
-        self.assertEqual(result.status_code, 200)
+    #     result = self.client.get('/listings.json')
+    #     self.assertEqual(result.status_code, 200)
+
+
+    # def test_calculator(self):
+    #     """ Test that the calculator route returns a response. """
+
+    #     result = self.client.get('/calculator')
+    #     self.assertEqual(result.status_code, 200)
+
 
     # def test_login(self):
     #     """ Test that the login page loads. """
@@ -74,11 +78,13 @@ class FlaskServerIntegrationTests(unittest.TestCase):
     #         follow_redirects=True)
     #     self.assertEqual(result.status_code, 200)
 
+
     # def test_registration_invalid(self):
     #     """ Test that the user cannot create an account if it already exists in the database. """
     #     result = self.client.post('/register')
     #     self.assertEqual(result.status_code, 200)
     #     # add assertIn
+
 
     # def test_account_details(self):
     #     """ Test that user account detail page loads. """
@@ -91,12 +97,11 @@ class FlaskDatabaseTests(unittest.TestCase):
 
     def setUp(self):
         """ Sets up a new client test app and database before each test. """
-
         self.client = app.test_client()
         app.config['TESTING'] = True
 
         # Connect to test database
-        connect_to_db_flask(app, "postgresql:///testdb")
+        connect_to_db_flask(app, 'postgresql:///testdb')
 
         # Create tables and add sample data
         db.create_all()
@@ -110,34 +115,67 @@ class FlaskDatabaseTests(unittest.TestCase):
         db.drop_all()
 
 
-    def test_get_avg_rent(self):
-        """Test rent averages."""
+    def test_user(self):
+        """ Make sure we can find a user. """
 
-        self.mock_bedrooms = 2
-        self.mock_bathrooms = 2
-        self.mock_sqft = 2000
-        self.mock_latlng_point = 'POINT(37.8212167 -122.4211727)'
-
-        self.assertEqual({ 'avg_rent_by_br': 4468, 'avg_rent_by_sqft': 6080 }, get_avg_rent(self.mock_bedrooms, self.mock_bathrooms, self.mock_sqft, self.mock_latlng_point))
+        glinda = User.query.filter(User.firstname == 'glinda').first()
+        self.assertEqual('glinda', glinda.firstname)
+        self.assertEqual('glindagood@example.com', glinda.email)
 
 
-   def test_add_listing_to_db(self):
-        """Test rent averages."""
+    def test_listing(self):
+        """ Make sure we can find a listing. """
 
-        self.mock_bedrooms = 3
-        self.mock_bathrooms = 2
-        self.mock_sqft = 2200
-        self.mock_zpid = 33039402
-        self.mock_street = '209 Yellow Brick Rd'
-        self.mock_city = 'Emerald City'
-        self.mock_state = 'EA'
-        self.mock_zipcode = 93240
-        self.mock_latitude = 37.7791402
-        self.mock_longitude = -122.5659901
-        self.mock_price = 1420975
-        self.mock_hoa = 550
+        listing = Listing.query.get(38478998)
+        self.assertEqual(38478998, listing.zpid)
+        self.assertEqual(350, listing.hoa)
+        self.assertEqual(980430, listing.price)
 
-        self.assertEqual({ 'avg_rent_by_br': 4468, 'avg_rent_by_sqft': 6080 }, add_listing_to_db(self.mock_bedrooms, self.mock_bathrooms, self.mock_sqft, self.mock_latitude, self.mock_longitude, self.mock_price, self.mock_zpid, self.mock_street, self.mock_city, self.mock_state, self.mock_zipcode, self.mock_hoa))
+
+    def test_unit_details(self):
+        """ Make sure we can find unit details. """
+
+        listing = Listing.query.get(38478998)
+        self.assertEqual(2.0, listing.unitdetails.bedrooms)
+        self.assertEqual(1190, listing.unitdetails.sqft)
+        self.assertEqual(37.7872375, listing.unitdetails.latitude)
+
+
+    def test_rentals(self):
+        """ Make sure we can find a rental. """
+
+        rental = Rental.query.filter(Rental.cl_id == '6007117642').first()
+        self.assertEqual(1885, rental.price)
+
+
+    # def test_get_avg_rent(self):
+    #     """Test rent averages."""
+
+    #     self.mock_bedrooms = 2
+    #     self.mock_bathrooms = 2
+    #     self.mock_sqft = 2000
+    #     self.mock_latlng_point = 'POINT(37.8212167 -122.4211727)'
+
+    #     self.assertEqual({ 'avg_rent_by_br': 4468, 'avg_rent_by_sqft': 6080 }, get_avg_rent(self.mock_bedrooms, self.mock_bathrooms, self.mock_sqft, self.mock_latlng_point))
+
+
+   # def test_add_listing_to_db(self):
+   #      """Test rent averages."""
+
+   #      self.mock_bedrooms = 3
+   #      self.mock_bathrooms = 2
+   #      self.mock_sqft = 2200
+   #      self.mock_zpid = 33039402
+   #      self.mock_street = '209 Yellow Brick Rd'
+   #      self.mock_city = 'Emerald City'
+   #      self.mock_state = 'EA'
+   #      self.mock_zipcode = 93240
+   #      self.mock_latitude = 37.7791402
+   #      self.mock_longitude = -122.5659901
+   #      self.mock_price = 1420975
+   #      self.mock_hoa = 550
+
+   #      self.assertEqual({ 'avg_rent_by_br': 4468, 'avg_rent_by_sqft': 6080 }, add_listing_to_db(self.mock_bedrooms, self.mock_bathrooms, self.mock_sqft, self.mock_latitude, self.mock_longitude, self.mock_price, self.mock_zpid, self.mock_street, self.mock_city, self.mock_state, self.mock_zipcode, self.mock_hoa))
 
 
 
